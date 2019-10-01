8dab1af36021a4710bee6dceecb918314c3cc5fb
Hamza Amer <hamer1563@gmail.com>
2019-09-30 21:43:06 -0400

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int num = 2;
+			if (num > 5) {
+				Console.WriteLine("You are old");
+		} else {
+				Console.WriteLine("ur not that old");
+   }
+			Console.WriteLine("My name is Hamza and I am " + num);
+
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("This is C#");
+			Console.ReadLine();
+		}
+	}
+}
