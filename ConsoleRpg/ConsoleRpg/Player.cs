9e568893810b7a355190c3764b235389697af601
Hamza Amer <hamer1563@gmail.com>
2019-10-02 20:54:44 -0400

    Add files via upload

---
+++ ConsoleRpg/ConsoleRpg/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRpg {
+    class Player {
+        public string name;
+        public int coins = 0;
+        public int health = 10;
+        public int damage = 1;
+        public int armorValue = 0;
+        public int potions = 5;
+    }
+}
---
+++ ConsoleRpg/ConsoleRpg/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpg {
+    class Program {
+
+        public static Player currentPlayer = new Player();
+        static void Main(string[] args) {
+            Start();
+        }
+
+        static void Start() {
+            Console.WriteLine("Console RPG");
+            Console.WriteLine("Name:");
+            currentPlayer.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("You awake in a cold dark room, you feel dazed and having trouble \n " +
+                "remembering who you are..");
+            if (currentPlayer.name.Length == 0) {
+                Console.WriteLine("You can't even remember your name.");
+            } else { 
+                Console.WriteLine("but you know your name is " + currentPlayer.name);
+
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("You wander around the darkness untill you find a door handle");
+        }
+    }
+}
